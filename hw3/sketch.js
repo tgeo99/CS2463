23a2918f130c5abb003fc8f6a0133c667907f7ea
tgeo99 <tgeo1zda2@gmail.com>
2023-02-07 19:21:04 -0600

    Re-did Assignment 3

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -1,96 +1,114 @@
-let spriteSheet;
-let walkingAnimation;
-let chazSheet;
-let chazAnimation;
-let walking2;
-let chazAnimation2;
-function preload() {
-  spriteSheet = loadImage("assets/SpelunkyGuy.png");
-  chazSheet = loadImage("assets/Chaz.png");
+let img1;
+function preload(){
+  img1 = loadImage("assets/22085.png");
+    //"MapleStory - Monsters (Perion) - Dark Axe Stump" by Boo.
+    //Borrowed from https://www.spriters-resource.com/fullview/22085/
+  img2 = loadImage("assets/Green.png");
+    //Spelunky - Green
+    //Borrowed from https://www.spriters-resource.com/pc_computer/spelunky/
 }
-
-function setup() {
-  createCanvas(400, 400);
+let Character1;
+let Character2;
+let Character3;
+function setup(){
+  createCanvas(600,400);
   imageMode(CENTER);
+  char1idle = new Anim(img1, 1, -1, 72, 80, 0, 0);
+  char1walk = new Anim(img1, 4, -1, 72, 80, 0, 81);
+  Character1 = new WalkingController(char1idle,char1walk, 100,100);
+  
+  char2idle = new Anim(img2, 1, 1, 80, 80, 0,0);
+  char2walk = new Anim(img2, 9, 1, 80, 80, 0,0);
+  Character2 = new WalkingController(char2idle,char2walk, 100,200);
 
-  walkingAnimation = new WalkingAnimation(spriteSheet,80,80,200,200,9);
-  walking2 = new WalkingAnimation(spriteSheet,80,80,280,280,9,1.5);
-  chazAnimation = new WalkingAnimation(chazSheet,12,30,200,100,3,2,183,7);
-  chazAnimation2 = new WalkingAnimation(chazSheet,12,30,100,100,3,1,183,7);
+  char3idle = new Anim(img2, 1, .5, 80, 80, 0,0);
+  char3walk = new Anim(img2, 9, .5, 80, 80, 0,0);
+  Character3 = new WalkingController(char3idle,char3walk, 200,200);
 }
 
-function draw() {
-  background(220);
-  
-  walkingAnimation.draw();
-  walking2.draw();
-  chazAnimation.draw();
-  chazAnimation2.draw();
+function draw(){
+  background(240);
+  Character1.draw();
+  Character2.draw();
+  Character3.draw();
 }
 
 function keyPressed() {
-  walkingAnimation.keyPressed(RIGHT_ARROW,LEFT_ARROW);
-  walking2.keyPressed(RIGHT_ARROW,LEFT_ARROW);
-  chazAnimation.keyPressed(RIGHT_ARROW,LEFT_ARROW);
-  chazAnimation2.keyPressed(RIGHT_ARROW,LEFT_ARROW);
+  Character1.keyPressed(RIGHT_ARROW, LEFT_ARROW);
+  Character2.keyPressed(RIGHT_ARROW, LEFT_ARROW);
+  Character3.keyPressed(RIGHT_ARROW, LEFT_ARROW);
 }
 
 function keyReleased() {
-  walkingAnimation.keyReleased(RIGHT_ARROW,LEFT_ARROW);
-  walking2.keyReleased(RIGHT_ARROW,LEFT_ARROW);
-  chazAnimation.keyReleased(RIGHT_ARROW,LEFT_ARROW);
-  chazAnimation2.keyReleased(RIGHT_ARROW,LEFT_ARROW);
+  Character1.keyReleased(RIGHT_ARROW, LEFT_ARROW);
+  Character2.keyReleased(RIGHT_ARROW, LEFT_ARROW);
+  Character3.keyReleased(RIGHT_ARROW, LEFT_ARROW);
 }
 
-class WalkingAnimation {
-  constructor(spritesheet, sw, sh, dx, dy, animationLength, aScale = 1, offsetX = 0, offsetY = 0) {
-    this.spritesheet = spritesheet;
-    this.sw = sw;
-    this.sh = sh;
-    this.dx = dx;
-    this.dy = dy;
-    this.u = 0;
-    this.v = 0;
-    this.animationLength = animationLength;
-    this.currentFrame = 0;
+class Anim{
+  constructor(spriteSheet, numFrames, scl, spriteWidth, spriteHeight, spriteOffsetX, spriteOffsetY){
+    this.spriteSheet = spriteSheet;
+    this.numFrames = numFrames;
+    this.scl = scl;
+    this.spriteWidth = spriteWidth;
+    this.spriteHeight = spriteHeight;
+    this.spriteOffsetX = spriteOffsetX;
+    this.spriteOffsetY = spriteOffsetY;
+    this.draw = function(u){
+      //           img       x y      dest width   dest height          x loc in img                         y loc in img          img width        img height
+      image(this.spriteSheet,0,0,this.spriteWidth,this.spriteHeight,u*this.spriteWidth+this.spriteOffsetX,this.spriteOffsetY,this.spriteWidth,this.spriteHeight);
+    }
+  }
+}
+
+class WalkingController {
+  //          anim  anim     x  y
+  constructor(idle, walking, x, y){
+    this.idle = idle;
+    this.walking = walking;
+    this.x = x;
+    this.y = y;
+    this.xDir = 1;
+    this.frame = 0;
     this.moving = 0;
-    this.xDirection = 1;
-    this.offsetX = offsetX;
-    this.offsetY = offsetY;
-    this.aScale = aScale;
-  }
 
-  draw() {
-
-    this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : 0;
-    push();
-    translate(this.dx,this.dy);
-    scale(this.xDirection * this.aScale, this.aScale);
-  
-    image(this.spritesheet,0,0,this.sw,this.sh,this.u*this.sw+this.offsetX,this.v*this.sh+this.offsetY,this.sw,this.sh);
-    pop();
-    if (frameCount % 6 == 0) {
-      this.currentFrame++;
+    this.draw = function(){
+      if(this.moving){
+        this.u = this.frame % this.walking.numFrames;
+        push();
+        translate(this.x,this.y);
+        scale(this.walking.scl * this.xDir, Math.abs(this.walking.scl));
+        this.walking.draw(this.u);
+        pop();
+      }
+      else{
+        this.u = this.frame % this.idle.numFrames;
+        push();
+        translate(this.x,this.y);
+        scale(this.idle.scl * this.xDir, Math.abs(this.idle.scl));
+        this.idle.draw(this.u);
+        pop();
+      }
+      if (frameCount % 7 == 0) {
+        this.frame++;
+      }
+      this.x += this.moving;
     }
-  
-    this.dx += this.moving;
-  }
-
-  keyPressed(right, left) {
-    if (keyCode === right) {
-      this.moving = 1;
-      this.xDirection = 1;
-      this.currentFrame = 1;
-    } else if (keyCode === left) {
-      this.moving = -1;
-      this.xDirection = -1;
-      this.currentFrame = 1;
+    this.keyPressed = function(right,left){
+      if(keyCode === right){
+        this.xDir = 1;
+        this.moving = 1;
+      }
+      if(keyCode === left){
+        this.xDir = -1;
+        this.moving = -1;
+      }
     }
-  }
-
-  keyReleased(right,left) {
-    if (keyCode === right || keyCode === left) {
-      this.moving = 0;
+    this.keyReleased = function(right, left){
+      if(keyCode === right || keyCode === left){
+        this.moving = 0;
+        this.frame = 0;
+      }
     }
   }
 }
